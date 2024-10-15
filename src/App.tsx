fa23bdbcdc2d2f5434a843bcf113ec880e0ef1bd
Chris Gregory <gregorybchris@gmail.com>
2024-10-14 23:58:03 -0700

    Add quicksand font

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,16 @@
+import { floydWarshall } from "./lib/algo";
+import { INF } from "./lib/math";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const graph = [
+    [0, 3, INF, 5],
+    [2, 0, INF, 4],
+    [INF, 1, 0, INF],
+    [INF, INF, 2, 0],
+  ];
+
+  const shortestPaths = floydWarshall(graph);
+  console.log("APSP: ", shortestPaths);
+
+  return <h1 className="text-xl font-quicksand">Hello world!</h1>;
 }
