86206e8923db4eaf4a433ee84af2a9f2bafd174c
Chris Gregory <gregorybchris@gmail.com>
2024-10-31 22:28:25 +0000

    Vector math

--- src/lib/math.ts
+++ src/lib/math.ts
@@ -15,3 +15,31 @@ export function logistic(a: number, b: number, x: number): number {
 export function logarithmic(a: number, b: number, x: number): number {
   return a * Math.log(x) + b;
 }
+
+export type Vector = {
+  x: number;
+  y: number;
+};
+
+export class Vec {
+  static add(v1: Vector, v2: Vector): Vector {
+    return { x: v1.x + v2.x, y: v1.y + v2.y };
+  }
+
+  static mult(v: Vector, s: number): Vector {
+    return { x: v.x * s, y: v.y * s };
+  }
+
+  static sub(v1: Vector, v2: Vector): Vector {
+    return { x: v1.x - v2.x, y: v1.y - v2.y };
+  }
+
+  static mag(v: Vector): number {
+    return Math.sqrt(v.x * v.x + v.y * v.y);
+  }
+
+  static normalize(v: Vector): Vector {
+    const mag = Vec.mag(v);
+    return Vec.mult(v, 1 / mag);
+  }
+}
